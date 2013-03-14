c001cadb8e78d5cf3357281aa59d4f72dc7d1e48
Wouter Hordijk <wouterhordijk@gmail.com>
2013-03-14 17:46:27 +0100

    several small fixes and started on new project of the hydrogen molecule and atom

--- hydrogen/atom/qmc.f90
+++ hydrogen/atom/qmc.f90
@@ -5,11 +5,11 @@ program qmc
     integer, parameter :: N = 400      ! number of walkers
     integer, parameter :: k = 26*10**3 ! number of walks
     integer, parameter :: m = 5        ! number of alpha values
-    real(8), parameter :: lower = 0.4  ! lower limit of alpha range
-    real(8), parameter :: upper = 0.6  ! upper limit of alpha range
+    real(8), parameter :: lower = 0.8  ! lower limit of alpha range
+    real(8), parameter :: upper = 1.2  ! upper limit of alpha range
     real(8), dimension(1, m) :: aarray
     real(8) :: a
-    real(8), dimension(1, N) :: x
+    real(8), dimension(3, N) :: x
     real(8) :: Eav
     real(8) :: var
     integer :: i
@@ -107,7 +107,7 @@ contains
         real(8), intent(in) :: a
         integer, intent(in) :: N
         real(8), dimension(:, :), intent(inout) :: x
-        real(8), dimension(1, N) :: delta_x
+        real(8), dimension(3, N) :: delta_x
         real(8), dimension(1, N) :: p
         real(8), dimension(1, N) :: u
         real(8), dimension(1, N) :: tf 
@@ -118,6 +118,8 @@ contains
         call random_number(u)
         tf = merge(1, 0, u < p)
         x(1, :) = x(1, :) + tf(1, :) * delta_x(1, :)
+        x(2, :) = x(2, :) + tf(1, :) * delta_x(2, :)
+        x(3, :) = x(3, :) + tf(1, :) * delta_x(3, :)
 
     end subroutine
 
@@ -127,7 +129,7 @@ contains
         real(8), dimension(:, :), intent(in) :: x
         real(8), dimension(:, :), intent(inout) :: E_L
 
-        E_L = a + (1 / 2d0 - 2d0 * a**2) * x**2
+        E_L(1, :) = - 1d0 / sqrt(sum(x**2, dim =1)) - 1d0 / 2 * a * ( a - 2d0 / sqrt(sum(x**2, dim = 1))) 
 
     end subroutine
 
@@ -135,9 +137,9 @@ contains
 
         real(8), intent(in) :: a
         real(8), dimension(:, :), intent(in) :: x
-        real(8), dimension(1, size(x)) :: psi
+        real(8), dimension(1, size(x, dim = 2)) :: psi
 
-        psi = exp(- a * x**2)
+        psi(1, :) = exp(- a * sqrt(sum(x**2, dim = 1)))
 
     end function
 
--- hydrogen/molecule/qmc.f90
+++ hydrogen/molecule/qmc.f90
@@ -2,58 +2,69 @@ program qmc
 
     implicit none
 
-    integer, parameter :: N = 400      ! number of walkers
-    integer, parameter :: k = 26*10**3 ! number of walks
-    integer, parameter :: m = 5        ! number of alpha values
-    real(8), parameter :: lower = 0.4  ! lower limit of alpha range
-    real(8), parameter :: upper = 0.6  ! upper limit of alpha range
-    real(8), dimension(1, m) :: aarray
-    real(8) :: a
-    real(8), dimension(1, N) :: x
+    integer, parameter :: N = 400           ! number of walkers
+    integer, parameter :: k = 26*10**3      ! number of walks
+    integer, parameter :: beta_m = 5        ! number of beta values
+	real(8), parameter :: beta_lower = 0.4  ! lower limit of beta range
+    real(8), parameter :: beta_upper = 0.7  ! upper limit of beta range
+    integer, parameter :: beta_m = 5        ! number of beta values
+    real(8), dimension(1, m) :: beta_array
+    real(8) :: beta
+	real(8), parameter :: s_lower = 1       ! lower limit of s range
+    real(8), parameter :: s_upper = 2       ! upper limit of s range
+    integer, parameter :: s_m = 5           ! number of s values
+    real(8), dimension(1, m) :: s_array
+    real(8) :: s
+    real(8), dimension(3, N) :: r1
+    real(8), dimension(3, N) :: r2
     real(8) :: Eav
     real(8) :: var
     integer :: i
 
-    call alpha_array(m, lower, upper, aarray)
+    beta_array = array(beta_m, beta_lower, beta_upper)
+    s_array = array(s_m, s_lower, s_upper)
     
     open (unit = 12, file = "energies.dat", status = "replace")
     
     do i = 1, m
         a = aarray(1, i)
         
-        call initialize(a, N, x)
-        call montecarlo(a, N, k, x, Eav, var)
+        call initialize(s, beta, N, r1, r2)
+        call montecarlo(s, beta, N, k, r1, r2, Eav, var)
         
-        write (12, *) a, Eav, var
+        write (12, *) s, beta, Eav, var
     end do
     
     close (unit = 12)
 
 contains
 
-    subroutine alpha_array(m, lower, upper, aarray)
+    function array(m, lower, upper)
 
         integer, intent(in) :: m
         real(8), intent(in) :: lower
         real(8), intent(in) :: upper
-        real(8), dimension (:, :), intent(inout) :: aarray
+        real(8), dimension (:, :), intent(out) :: array
         integer :: i
 
         do i = 1, m
-            aarray(1, i) = (i - 1) * (upper - lower) / (m - 1) + lower
+            array(1, i) = (i - 1) * (upper - lower) / (m - 1) + lower
         end do
 
-    end subroutine
+    end function
 
-    subroutine initialize(a, N, x)
+    subroutine initialize(s, beta, N, r1, r2)
 
-        real(8), intent(in) :: a
+        real(8), intent(in) :: s
+        real(8), intent(in) :: beta
         integer, intent(in) :: N
-        real(8), dimension(:, :), intent(inout) :: x
+        real(8), dimension(:, :), intent(inout) :: r1
+        real(8), dimension(:, :), intent(inout) :: r2
         integer :: i
 
         call init_random_seed()
-        call random_number(x)
+        call random_number(r1)
+        call random_number(r2)
 
         do i = 1, 4000
             call metropolis(a, N, x)
@@ -107,17 +118,19 @@ contains
         real(8), intent(in) :: a
         integer, intent(in) :: N
         real(8), dimension(:, :), intent(inout) :: x
-        real(8), dimension(1, N) :: delta_x
+        real(8), dimension(3, N) :: delta_x
         real(8), dimension(1, N) :: p
         real(8), dimension(1, N) :: u
         real(8), dimension(1, N) :: tf 
 
         call random_number(delta_x)
         delta_x = 1d0 * (delta_x(:, :) - 0.5d0)
-        p = min(psi(a, x + delta_x)**2 / psi(a, x)**2 , 1d0)
+        p = min(psi(a, x + delta_x) / psi(a, x) , 1d0)
         call random_number(u)
         tf = merge(1, 0, u < p)
         x(1, :) = x(1, :) + tf(1, :) * delta_x(1, :)
+        x(2, :) = x(2, :) + tf(1, :) * delta_x(2, :)
+        x(3, :) = x(3, :) + tf(1, :) * delta_x(3, :)
 
     end subroutine
 
@@ -127,7 +140,7 @@ contains
         real(8), dimension(:, :), intent(in) :: x
         real(8), dimension(:, :), intent(inout) :: E_L
 
-        E_L = a + (1 / 2d0 - 2d0 * a**2) * x**2
+        E_L(1, :) = 3d0 * a + (1 / 2d0 - 2d0 * a**2) * sum(x**2, dim = 1)
 
     end subroutine
 
@@ -135,9 +148,9 @@ contains
 
         real(8), intent(in) :: a
         real(8), dimension(:, :), intent(in) :: x
-        real(8), dimension(1, size(x)) :: psi
+        real(8), dimension(1, size(x, dim = 2)) :: psi
 
-        psi = exp(- a * x**2)
+        psi(1, :) = exp(-2d0 * a * sum(x**2, dim = 1))
 
     end function
 
